a1e3dd4e6bf81444a5124adf4120347bca7e27cb
Tomáš Sabol <tomassabol2@gmail.com>
2021-02-12 14:44:06 +0100

    Create app.py

---
+++ python/app.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from bs4 import BeautifulSoup
+import requests
+
+__author__ = "Tomas Sabol, https://www.github.com/tomassabol"
+
+root = Tk()
+root.title("myWeather")
+root.geometry("570x1212")
+
+root.mainloop()
