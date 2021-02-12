d57843183772a3dd21e69b4b3cf3676d17bfce73
Tomáš Sabol <tomassabol2@gmail.com>
2021-02-12 17:14:39 +0100

    Update app.py

--- python/app.py
+++ python/app.py
@@ -1,20 +1,60 @@
 from tkinter import *
-from bs4 import BeautifulSoup
+from PIL import ImageTk,Image
+from configparser import ConfigParser
 import requests
 
-__author__ = "Tomas Sabol, https://www.github.com/tomassabol"
+url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
 
-# setup window
-root = Tk()
+config_file = "config.ini"
+config = ConfigParser()
+config.read(config_file)
+api_key = config["api_key"]["key"]
+
+# functions
+
+def get_weather():
+    result = requests.get(url.format(city, api_key))
+    if result:
+         json = result.json()
+         city = json["name"]
+         country = json["sys"]["country"]
+         temp_kelvin
+    else:
+        return None
+
+
+get_weather("London")
+
+
+def search():
+    pass
+
+
+# window setup
+root=Tk()
 root.title("myWeather")
 root.geometry("570x1212")
 
 
-# var
-background_image=PhotoImage("https://raw.githubusercontent.com/tomassabol/myWeather/main/python/images/sunny.png")
-background_label = Label(root, image=background_image)
-background_label.place(x=0, y=0, relwidth=1, relheight=1)
+#  ui
+city_text = StringVar
+city_text = Entry(root, textvariable=city_text)
+city_text.pack()
 
+search_btn = Button(root, text="Search", width=10, command=search)
+search_btn.pack()
+
+location_label = Label(root, text="Location", font=("SF Pro", 40, "bold"))
+location_label.pack()
+
+image_label = Label(root)
+image_label.pack()
+
+temperature_label = Label(root, text="temperature", font=("SF Pro", 12, "bold"))
+temperature_label.pack()
+
+weather_label = Label(root, text="weather", font=("SF Pro", 12, "bold"))
+weather_label.pack()
 
 
 root.mainloop()
