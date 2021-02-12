17ee1629e3a964d9661b74a602a3a064c9a2b858
Tomáš Sabol <tomassabol2@gmail.com>
2021-02-12 15:03:46 +0100

    Update app.py

--- python/app.py
+++ python/app.py
@@ -4,8 +4,17 @@ import requests
 
 __author__ = "Tomas Sabol, https://www.github.com/tomassabol"
 
+# setup window
 root = Tk()
 root.title("myWeather")
 root.geometry("570x1212")
+
+
+# var
+background_image=PhotoImage("https://raw.githubusercontent.com/tomassabol/myWeather/main/python/images/sunny.png")
+background_label = Label(root, image=background_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+
 
 root.mainloop()
